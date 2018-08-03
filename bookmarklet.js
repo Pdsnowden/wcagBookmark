315369bd5ba2449d26dba106c3cc7eaa81b805b9
Paul Snowden <paulsnowden@outlook.com>
2018-08-03 11:59:48 -0700

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -16,7 +16,7 @@ javascript:(
         document.getElementsByTagName("head")[0].appendChild(fileRef);
 
         fileRef = document.createElement("script");
-        fileRef.src = "http://localhost/cas233w/week4/lab/wcag.js";
+        fileRef.src = "http://wcag.js";
         fileRef.type = "text/javascript";
         document.getElementsByTagName("head")[0].appendChild(fileRef);
       }else {
